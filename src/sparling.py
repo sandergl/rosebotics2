8458f7f51e9b7fd93c8e6fc45ae856983158ba99
sparlisb <sparlisb@rose-hulman.edu>
2018-10-25 16:53:11 -0400

    color sensor thing

--- src/sparling.py
+++ src/sparling.py
@@ -6,9 +6,18 @@
 import rosebotics as rb
 import time
 
-#test
+
 def main():
     """ Runs YOUR specific part of the project """
+    color = rb.Color.BLUE.value
+    move_to_color(color)
+
+
+def move_to_color(color):
+    robot = rb.Snatch3rRobot()
+    robot.drive_system.start_moving(50, 50)
+    robot.color_sensor.wait_until_color_is(color)
+    robot.drive_system.stop_moving()
 
 
 main()
