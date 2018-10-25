b5c96c31cad84a0c8136eea87ce5e8b268c44812
williae2 <williae2@rose-hulman.edu>
2018-10-25 16:46:38 -0400

    Wow! I have begun to write my code! It's so exciting! My code makes the robot follow the black line!

--- src/Williams.py
+++ src/Williams.py
@@ -1,5 +1,5 @@
 """
-  Capstone Project.  Code written by PUT_YOUR_NAME_HERE.
+  Capstone Project.  Code written by Elijah Williams.
   Fall term, 2018-2019.
 """
 
@@ -9,6 +9,13 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-    def make_shape():
+    follow_line()
+def follow_line():
+    robot = rb.Snatch3rRobot()
+    robot.color_sensor.get_color()
+    while True:
+        robot.drive_system.start_moving(100, 100)
+        if robot.color_sensor.get_color() == 6:
+            robot.drive_system.turn_degrees(1, 100)
 
 main()
