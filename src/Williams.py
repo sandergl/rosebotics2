577e67ac69861c5a2b58fd4a3d9736823d685ce7
williae2 <williae2@rose-hulman.edu>
2018-10-28 15:40:00 -0400

    Wow! I have begun to write my code! It's so exciting! My code makes the robot follow the black line!

--- src/Williams.py
+++ src/Williams.py
@@ -10,12 +10,14 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
     follow_line()
+
 def follow_line():
     robot = rb.Snatch3rRobot()
-    robot.color_sensor.get_color()
+    robot.color_sensor.get_reflected_intensity()
     while True:
-        robot.drive_system.start_moving(100, 100)
-        if robot.color_sensor.get_color() == 6:
-            robot.drive_system.turn_degrees(1, 100)
+        robot.drive_system.start_moving(50, 50)
+        if robot.color_sensor.get_reflected_intensity() >5:
+            robot.drive_system.turn_degrees(10, 50)
+
 
 main()
