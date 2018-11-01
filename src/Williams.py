a982984b646013b820e7800ee0ed008df4cbe822
williae2 <williae2@rose-hulman.edu>
2018-11-01 16:11:38 -0400

    Wow! Now the robot has to recognize color! It even beeps!

--- src/Williams.py
+++ src/Williams.py
@@ -3,16 +3,17 @@
   Fall term, 2018-2019.
 """
 
-import rosebotics as rb
 import time
-
+import ev3dev.ev3 as ev3
+import rosebotics_new as rb2
 
 def main():
     """ Runs YOUR specific part of the project """
-    follow_line()
+    #follow_line()
 
+    beep_for_color()
 def follow_line():
-    s8n = rb.Snatch3rRobot()
+    s8n = rb2.Snatch3rRobot()
     s8n.color_sensor.get_color()
     while True:
         s8n.drive_system.start_moving(100, 100)
@@ -20,6 +21,12 @@ def follow_line():
             print('Nota res mala, optima.')
             s8n.drive_system.stop_moving()
             s8n.drive_system.turn_degrees(-15, 100)
+def beep_for_color():
+    camera = rb2.Camera()
+    camera.set_signature()
+    blank = camera.get_biggest_blob()
+    if camera.get_biggest_blob() > blank:
+        ev3.Sound.beep().wait()
 
 
 main()
